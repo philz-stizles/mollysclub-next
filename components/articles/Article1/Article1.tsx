1ce0d83364574664c366830dcdd2b49045214baa
philz-stizles <theophilusighalo@gmail.com>
2021-11-14 21:04:26 +0100

    Fix type bug 11

--- components/articles/Article1/Article1.tsx
+++ components/articles/Article1/Article1.tsx
@@ -1,20 +1,30 @@
-// import React from 'react'
-// import Link from 'next/link'
+import React from 'react'
+import Link from 'next/link'
 
-// const Article1 = ({ title, content, linkRef, linkName }) => {
-//   return (
-//     <div className="Article1">
-//       <div>
-//         <h1>{title}</h1>
-//         <p>{content}</p>
-//       </div>
-//       <div>
-//         <Link href={linkRef}>
-//           <a>{linkName}</a>
-//         </Link>
-//       </div>
-//     </div>
-//   )
-// }
+const Article1 = ({
+  title,
+  content,
+  linkRef,
+  linkName,
+}: {
+  title: string
+  content: string
+  linkName: string
+  linkRef: string
+}) => {
+  return (
+    <div className="Article1">
+      <div>
+        <h1>{title}</h1>
+        <p>{content}</p>
+      </div>
+      <div>
+        <Link href={linkRef}>
+          <a>{linkName}</a>
+        </Link>
+      </div>
+    </div>
+  )
+}
 
-// export default Article1
+export default Article1
